be4953987fec83b488881a69c29af8bf32ef7c2c
Saurabh Krishan <129500174+Saurabhmurari98@users.noreply.github.com>
2024-03-24 18:11:58 +0530

    Create Const.cs

---
+++ Constant/Const.cs
@@ -0,0 +1,10 @@
+using System.Security.Cryptography.X509Certificates;
+namespace Portfolio.Constant
+{
+    public class Consts
+    {
+        public const string Server = "mongodb://localhost:27017";
+        public const string Database = "Portfolio";
+        public const string Collection = "ContactUs";
+    }
+}
